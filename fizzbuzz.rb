f3d7fed8e47ba28294f62af89550bb7c742e9aad
Sam Tamburri <tamburrisam@gmail.com>
2018-09-14 20:08:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 	def fizzbuzz(int)
   if int % 3 == 0 and int % 5 == 0
@@ -9,3 +10,20 @@
    end
  end
  
+=======
+require_relative './spec_helper.rb'
+
+	def fizzbuzz(int)
+  if int % 3 == 0 
+    return "Fizz"
+    
+ elsif int % 5 == 0  
+    return "Buzz"
+    
+ elsif int % 3 && int % 5 == 0 
+    return "FizzBuzz"
+    
+ elsif
+
+  
+>>>>>>> cdb152dc909fde3546b28aba301fd8c76b34bb8b
